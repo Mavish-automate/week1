a4f1e56e5b2bebaeb90c38fa25ab20e33c5dea41
Mavish-automate <128235008+Mavish-automate@users.noreply.github.com>
2023-07-17 22:29:42 +0530

    Add files via upload
    week1day1 Class Firefox  printing the variables in console

---
+++ Firefox.java
@@ -0,0 +1,18 @@
+package week1Day1;
+
+public class Firefox 
+{
+public static void main(String[] args) 
+{
+	float version=100.2f;
+	String name="firefox";
+	boolean visible=true;
+	int year=1998;
+	char logo='m';
+	System.out.println("version is : "+" "+version);
+	System.out.println("name :"+" "+name);
+	System.out.println("visibility check "+" "+visible);
+	System.out.println("year :"+" "+year);
+	System.out.println("logo : "+" "+logo);
+}
+}
